da009f1e00431e200db305365d514e3c5d69ecf7
Medeya <dea.2412@gmail.com>
2024-02-10 14:36:33 +0300

    initial structure

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    background: {
+        dark: '#484283',
+        light: '#FFFFFF',
+        secondary: '#FFF4EE',
+        accent: '#F14F4F',
+    },
+    border: '#DBBBA9',
+    text: {
+        dark: '#444B58',
+        light: '#FFFFFF',
+    },
+  };
